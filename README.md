73739ab8d5850522c71ee58b41eca4bb3cfd049c
Daniel Lanza Garcia <dlanza1@gmail.com>
2016-03-17 17:43:42 +0100

    Huge improvements!

--- README.md
+++ README.md
@@ -9,27 +9,110 @@ bin/compile
 Usage:
 
 ``` 
-bin/run <path>
+bin/run <path> [<max_number_of_blocks_to_print>]
 ```
 
 Example output:
 
 ```
-[dlanza@itrac1511 hdfs-meta]$ bin/run /user/hloader/SCADAR/VAC_LHC_LOAD.db/eventhistory_00100001_f/part-m-00000.avro
+[dlanza@itrac1505 hdfs-meta]$ bin/run /user/hloader/SCADAR/SURVEY_F3_LOAD.db/eventhistory_00100001 2
 
-Showing information fo filer: hdfs://p01001532067275.cern.ch/user/hloader/SCADAR/VAC_LHC_LOAD.db/eventhistory_00100001_f/part-m-00000.avro
-	isDirectory: false
-	isFile: true
+Showing metadata for: hdfs://p01001532067275.cern.ch/user/hloader/SCADAR/SURVEY_F3_LOAD.db/eventhistory_00100001
+	isDirectory: true
+	isFile: false
 	isSymlink: false
 	encrypted: false
-	length: 9046982642
-	replication: 3
-	blocksize: 268435456
-	modification_time: Tue Mar 15 09:04:03 CET 2016
-	access_time: Wed Mar 16 15:54:10 CET 2016
+	length: 0
+	replication: 0
+	blocksize: 0
+	modification_time: Tue Mar 15 13:48:08 CET 2016
+	access_time: Thu Jan 01 01:00:00 CET 1970
 	owner: hloader
 	group: supergroup
-	permission: rw-r--r--
+	permission: rwxr-xr-x
+
+Collecting block locations...
+Collected 269 locations.
+
+16/03/17 17:41:56 INFO Configuration.deprecation: dfs.data.dir is deprecated. Instead, use dfs.datanode.data.dir
+Data directories and disk ids
+  DiskId: 0  Directory: FILE:/data01/hadoop/hdfs/data
+  DiskId: 1  Directory: FILE:/data1/hadoop/hdfs/data
+  DiskId: 2  Directory: FILE:/data10/hadoop/hdfs/data
+  DiskId: 3  Directory: FILE:/data11/hadoop/hdfs/data
+  DiskId: 4  Directory: FILE:/data12/hadoop/hdfs/data
+  DiskId: 5  Directory: FILE:/data13/hadoop/hdfs/data
+  DiskId: 6  Directory: FILE:/data14/hadoop/hdfs/data
+  DiskId: 7  Directory: FILE:/data15/hadoop/hdfs/data
+  DiskId: 8  Directory: FILE:/data16/hadoop/hdfs/data
+  DiskId: 9  Directory: FILE:/data17/hadoop/hdfs/data
+  DiskId: 10  Directory: FILE:/data18/hadoop/hdfs/data
+  DiskId: 11  Directory: FILE:/data19/hadoop/hdfs/data
+  DiskId: 12  Directory: FILE:/data2/hadoop/hdfs/data
+  DiskId: 13  Directory: FILE:/data20/hadoop/hdfs/data
+  DiskId: 14  Directory: FILE:/data21/hadoop/hdfs/data
+  DiskId: 15  Directory: FILE:/data22/hadoop/hdfs/data
+  DiskId: 16  Directory: FILE:/data23/hadoop/hdfs/data
+  DiskId: 17  Directory: FILE:/data24/hadoop/hdfs/data
+  DiskId: 18  Directory: FILE:/data25/hadoop/hdfs/data
+  DiskId: 19  Directory: FILE:/data26/hadoop/hdfs/data
+  DiskId: 20  Directory: FILE:/data27/hadoop/hdfs/data
+  DiskId: 21  Directory: FILE:/data28/hadoop/hdfs/data
+  DiskId: 22  Directory: FILE:/data29/hadoop/hdfs/data
+  DiskId: 23  Directory: FILE:/data3/hadoop/hdfs/data
+  DiskId: 24  Directory: FILE:/data30/hadoop/hdfs/data
+  DiskId: 25  Directory: FILE:/data31/hadoop/hdfs/data
+  DiskId: 26  Directory: FILE:/data32/hadoop/hdfs/data
+  DiskId: 27  Directory: FILE:/data33/hadoop/hdfs/data
+  DiskId: 28  Directory: FILE:/data34/hadoop/hdfs/data
+  DiskId: 29  Directory: FILE:/data35/hadoop/hdfs/data
+  DiskId: 30  Directory: FILE:/data36/hadoop/hdfs/data
+  DiskId: 31  Directory: FILE:/data37/hadoop/hdfs/data
+  DiskId: 32  Directory: FILE:/data38/hadoop/hdfs/data
+  DiskId: 33  Directory: FILE:/data39/hadoop/hdfs/data
+  DiskId: 34  Directory: FILE:/data4/hadoop/hdfs/data
+  DiskId: 35  Directory: FILE:/data40/hadoop/hdfs/data
+  DiskId: 36  Directory: FILE:/data41/hadoop/hdfs/data
+  DiskId: 37  Directory: FILE:/data42/hadoop/hdfs/data
+  DiskId: 38  Directory: FILE:/data43/hadoop/hdfs/data
+  DiskId: 39  Directory: FILE:/data44/hadoop/hdfs/data
+  DiskId: 40  Directory: FILE:/data45/hadoop/hdfs/data
+  DiskId: 41  Directory: FILE:/data46/hadoop/hdfs/data
+  DiskId: 42  Directory: FILE:/data47/hadoop/hdfs/data
+  DiskId: 43  Directory: FILE:/data48/hadoop/hdfs/data
+  DiskId: 44  Directory: FILE:/data5/hadoop/hdfs/data
+  DiskId: 45  Directory: FILE:/data6/hadoop/hdfs/data
+  DiskId: 46  Directory: FILE:/data7/hadoop/hdfs/data
+  DiskId: 47  Directory: FILE:/data8/hadoop/hdfs/data
+  DiskId: 48  Directory: FILE:/data9/hadoop/hdfs/data
+
+16/03/17 17:41:57 WARN DistributedFileSystemMetadata: list of data nodes could not be got from API (requieres higher privilegies).
+16/03/17 17:41:57 WARN DistributedFileSystemMetadata: getting datanode list from configuration file (may contain data nodes which are not active).
+
+ === Distribution along nodes and disks ===
+
+DiskId                   0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 4 4
+                         0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 Unknown   Count     Average
+Host
+itrac1508.cern.ch        0 = 0 = 0 = = = = 0 0 = 0 = 0 = = 0 = = + = = = + + = = = = = 0 = = + = = = = = 0 = = = = = = = = 0         77        1
+itrac1512.cern.ch        0 = + = = = 0 = = 0 0 = 0 = = = 0 = 0 = = = = 0 = = = 0 = 0 = 0 + = = = = = = = 0 + = = = = = = = 0         69        1
+lxfsrb53a07.cern.ch      0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0         0         0
+lxfsrb53a04.cern.ch      0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0         0         0
+p05153074365419.cern.ch  0 = = = = = 0 = = = 0 0 0 = 0 = = 0 0 = = 0 = 0 0 0 = 0 = 0 = 0 0 0 0 = 0 0 0 = 0 = = 0 0 = 0 = = 0         29        0
+itrac1502.cern.ch        0 = = 0 = 0 0 = 0 = 0 = 0 = 0 0 = = = 0 0 = 0 = = + + = = = = = = 0 0 = = = 0 0 0 0 = 0 = = 0 = 0 0         39        0
+itrac1501.cern.ch        0 = = = 0 = = = = 0 = = = = = 0 + + = = = = = = 0 = = = = = = = + = = = = 0 = = = = + = = = = = 0 0         106       2
+itrac1507.cern.ch        0 = = 0 = 0 0 = = 0 0 = 0 = = = = = 0 = = + = = + = = = = = = = 0 = 0 = = 0 = = 0 = = 0 + = = = = 0         66        1
+itrac1509.cern.ch        0 0 = 0 = = 0 = = 0 = = = 0 = 0 = 0 = = = = 0 0 = 0 = = = = = 0 0 = 0 = = 0 0 = 0 0 = 0 = 0 0 = 0 0         33        0
+lxfsrk39a01.cern.ch      0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0         0         0
+itrac1504.cern.ch        0 0 = = = = = = = 0 = = 0 = = = = 0 = 0 = = = 0 = = = 0 = = = = = 0 0 = 0 = 0 = = = = 0 = = = = 0 0         56        1
+itrac1510.cern.ch        0 = 0 0 = 0 0 0 = = = 0 0 = = 0 = = = 0 = = = 0 = 0 0 = = = 0 0 0 0 0 0 = = 0 = = 0 = = = 0 + = = 0         37        0
+itrac1506.cern.ch        0 0 0 = 0 = 0 = = 0 0 0 = 0 = = = = = 0 = = = = 0 = = = = 0 = = = 0 = = = = 0 + = = + + + = = = 0 0         68        1
+p05153074600927.cern.ch  0 0 = 0 0 0 0 0 = 0 = 0 0 = = 0 = 0 = = = 0 = 0 = = = 0 = 0 0 0 0 0 0 0 = 0 0 = = 0 = 0 0 = = = 0 0         26        0
+itrac1503.cern.ch        0 = = = = = 0 0 0 = = 0 = = 0 0 = 0 + = = = = = = = = 0 = = = = = 0 0 = = = = = = = = 0 = = = = = 0         61        1
+itrac1511.cern.ch        0 = = = = = 0 = = = 0 0 = 0 = = 0 = = 0 0 = = 0 = + = = = = = 0 = = = = = = 0 + = = = = 0 = = = 0 0         64        1
+itrac1505.cern.ch        0 0 = = 0 0 + = = 0 0 = = 0 0 = 0 0 = = = 0 = 0 0 = = 0 = = = = 0 0 = = = 0 = = 0 = = 0 = + = = 0 0         42        0
+
+ === Metadata of blocks and replicass ===
 
 Block (0) info:
 	Offset: 0
@@ -37,18 +120,18 @@ Block (0) info:
 	No cached hosts
 	Hosts:
 		Replica (0):
-			Host: itrac1509.cern.ch
-			Location: FILE:/data43/hadoop/hdfs/data (DiskId: 38)
-			Name: 128.142.210.237:1004
-			TopologyPaths: /0513_R-0050/RL21/128.142.210.237:1004
+			Host: itrac1507.cern.ch
+			Location: FILE:/data30/hadoop/hdfs/data (DiskId: 24)
+			Name: 128.142.210.206:1004
+			TopologyPaths: /0513_R-0050/RL05/128.142.210.206:1004
 		Replica (1):
-			Host: itrac1502.cern.ch
-			Location: FILE:/data47/hadoop/hdfs/data (DiskId: 42)
-			Name: 128.142.210.238:1004
-			TopologyPaths: /0513_R-0050/RL17/128.142.210.238:1004
+			Host: itrac1503.cern.ch
+			Location: FILE:/data2/hadoop/hdfs/data (DiskId: 12)
+			Name: 128.142.210.232:1004
+			TopologyPaths: /0513_R-0050/RL17/128.142.210.232:1004
 		Replica (2):
 			Host: itrac1504.cern.ch
-			Location: FILE:/data42/hadoop/hdfs/data (DiskId: 37)
+			Location: FILE:/data34/hadoop/hdfs/data (DiskId: 28)
 			Name: 128.142.210.236:1004
 			TopologyPaths: /0513_R-0050/RL17/128.142.210.236:1004
 
@@ -58,11 +141,20 @@ Block (1) info:
 	No cached hosts
 	Hosts:
 		Replica (0):
+			Host: itrac1507.cern.ch
+			Location: FILE:/data19/hadoop/hdfs/data (DiskId: 11)
+			Name: 128.142.210.206:1004
+			TopologyPaths: /0513_R-0050/RL05/128.142.210.206:1004
+		Replica (1):
 			Host: itrac1509.cern.ch
-			Location: FILE:/data25/hadoop/hdfs/data (DiskId: 18)
+			Location: FILE:/data7/hadoop/hdfs/data (DiskId: 46)
 			Name: 128.142.210.237:1004
 			TopologyPaths: /0513_R-0050/RL21/128.142.210.237:1004
-		Replica (1):
-			Host: itrac1507.cern.ch
-			...
+		Replica (2):
+			Host: itrac1512.cern.ch
+			Location: FILE:/data25/hadoop/hdfs/data (DiskId: 18)
+			Name: 128.142.210.235:1004
+			TopologyPaths: /0513_R-0050/RL21/128.142.210.235:1004
+
+16/03/17 17:41:57 WARN DistributedFileSystemMetadata: Not showing more blocks because limit has been reached
 ```
---
+++ src/main/java/DistributedFileSystemMetadata.java
@@ -0,0 +1,426 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map.Entry;
+
+import org.apache.hadoop.fs.BlockLocation;
+import org.apache.hadoop.fs.BlockStorageLocation;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.VolumeId;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.HdfsConfiguration;
+import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
+import org.apache.hadoop.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DistributedFileSystemMetadata extends DistributedFileSystem{
+	
+	private static final Logger LOG = LoggerFactory.getLogger(DistributedFileSystemMetadata.class);
+	
+	public static final int SIZEOF_INT = Integer.SIZE / Byte.SIZE;
+	
+	private static final int MAX_NUMBER_OF_LOCATIONS = 20000;
+	
+	private static int limitPrintedBlocks = 20;
+
+	public DistributedFileSystemMetadata() throws IOException{
+		HdfsConfiguration conf = new HdfsConfiguration();
+		
+		initialize(getDefaultUri(conf), conf);
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		if (args.length == 0 || args.length > 2) {
+			LOG.error("You need to specify a path as first argument. ");
+			System.exit(1);
+		}
+		if(args.length == 2){
+			limitPrintedBlocks = Integer.parseInt(args[1]);
+		}
+		
+		Path path = new Path(args[0]);
+		
+		@SuppressWarnings("resource")
+		DistributedFileSystemMetadata fsm = new DistributedFileSystemMetadata();
+		
+		fsm.printFileStatus(path);
+		
+		LinkedList<BlockLocation> blockLocations = fsm.getBlockLocations(path);
+		
+		String[] dataDirs = fsm.getDataDirs();
+		if(dataDirs != null){
+			System.out.println("Data directories and disk ids");
+			for (int i = 0; i < dataDirs.length; i++) {
+				System.out.println("  DiskId: " + i + "  Directory: " + dataDirs[i]);
+			}
+		}
+		System.out.println();
+		
+		if(fsm.getConf().getBoolean("dfs.datanode.hdfs-blocks-metadata.enabled", false)){
+			BlockStorageLocation[] blockStorageLocations = fsm.getFileBlockStorageLocations(blockLocations);
+			
+			HashMap<String, HashMap<Integer, Integer>> hosts_diskIds = 
+					fsm.calculateHostsDiskIdsCount(blockStorageLocations);
+			
+			String[] dataNodes = fsm.getDataNodes();
+			for (String name : dataNodes) {
+				if(!hosts_diskIds.containsKey(name))
+					hosts_diskIds.put(name, new HashMap<Integer, Integer>());
+			}
+			
+			fsm.printNodeDisksDistribution(hosts_diskIds, dataDirs);
+			
+			blockLocations.clear();
+			blockLocations.addAll(Arrays.asList(blockStorageLocations));
+		}else{
+			LOG.error("VolumnId/DiskId can not be collected since "
+					+ "dfs.datanode.hdfs-blocks-metadata.enabled is not enabled. So block distribution can not be shown.");
+		}
+		
+		fsm.printBlockMetadata(blockLocations, dataDirs);
+	}
+
+	private String[] getDataNodes() {
+
+		try {
+			DatanodeInfo[] dataNodeStats = getDataNodeStats();
+			
+			String[] hosts = new String[dataNodeStats.length];
+			for (int i = 0; i < hosts.length; i++)
+				hosts[i] = dataNodeStats[i].getHostName();
+			
+			return hosts;
+		} catch (IOException e) {
+			LOG.warn("list of data nodes could not be got from API (requieres higher privilegies).");
+		}
+		
+		try {
+			LOG.warn("getting datanode list from configuration file (may contain data nodes which are not active).");
+			return getDataNodesFromConf();
+		} catch (IOException e) {
+		}
+		
+		return new String[0];
+	}
+
+	private String[] getDataNodesFromConf() throws IOException {
+		InputStream in = getClass().getResourceAsStream("/dfs.includes");
+		if(in == null)
+			throw new IOException("File dfs.includes not found in classpath");
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        
+		LinkedList<String> hostnames = new LinkedList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+        	hostnames.add(line);
+        }
+        
+        reader.close();
+		
+		return hostnames.toArray(new String[hostnames.size()]);
+	}
+
+	private void printBlockMetadata(LinkedList<BlockLocation> blockLocations, String[] dataDirs) 
+			throws IOException {
+		
+		System.out.println();
+		System.out.println(" === Metadata of blocks and replicass ===");
+		System.out.println();
+		
+		int j = 0;
+		for (BlockLocation blockLocation : blockLocations) {
+			System.out.println("Block (" + j + ") info:");
+			
+			printBlockMetadata(blockLocation, dataDirs);
+			
+			j++;
+			
+			System.out.println();
+			
+			if(j >= limitPrintedBlocks){
+				LOG.warn("Not showing more blocks because limit has been reached");
+				break;
+			}
+		}
+		
+		System.out.println();
+	}
+
+	private void printNodeDisksDistribution(HashMap<String, HashMap<Integer, Integer>> hosts_diskIds, String[] dataDirs) {
+		System.out.println();
+		System.out.println(" === Distribution along nodes and disks ===");
+		System.out.println();
+		
+		if(dataDirs == null){
+			LOG.error("Cannot be calculated due to data directories configuration parameter are not available.");
+		}
+		
+		System.out.print(adjustLength("DiskId", 25));
+		if(dataDirs.length >= 10){
+			for (int i = 0; i < dataDirs.length; i++) {
+				System.out.print(adjustLength(Integer.toString(i / 10), 2));
+			}
+			System.out.println();
+			System.out.print(adjustLength("", 25));
+		}
+		for (int i = 0; i < dataDirs.length; i++) {
+			System.out.print(adjustLength(Integer.toString(i % 10), 2));
+		}
+		System.out.print(adjustLength("Unknown", 10));
+		System.out.print(adjustLength("Count", 10));
+		System.out.println(adjustLength("Average", 10));
+		System.out.println("Host");
+		for (Entry<String, HashMap<Integer, Integer>> host_diskIds : hosts_diskIds.entrySet()) {
+			System.out.print(adjustLength(host_diskIds.getKey(), 25));
+			
+			HashMap<Integer, Integer> diskIds_count = host_diskIds.getValue();
+			
+			float sum = 0;
+			for (int i = 0; i < dataDirs.length; i++){
+				Integer count = diskIds_count.get(i);
+				
+				if(count != null)
+					sum += diskIds_count.get(i);
+			}
+			float avg = sum / dataDirs.length;
+			
+			float low = (float) (avg - 0.2 * avg);
+			if(avg - low < 2)
+				low = avg - 2;			
+			float high = (float) (avg + 0.2 * avg);
+			if(high - avg < 2)
+				high = avg + 2;
+			
+			for (int i = 0; i < dataDirs.length; i++) {
+				Integer count = diskIds_count.get(i);
+				
+				if(count == null)
+					System.out.print(adjustLength("0", 2));
+				else if(count < low)
+					System.out.print(adjustLength("-", 2));
+				else if(count > high)
+					System.out.print(adjustLength("+", 2));
+				else
+					System.out.print(adjustLength("=", 2));
+			}
+			
+			Integer count_unk = diskIds_count.get(-1);
+			if(count_unk == null)
+				System.out.print(adjustLength("0", 10));
+			else
+				System.out.print(adjustLength(count_unk+"", 10));
+			
+			System.out.print(adjustLength(((int)sum)+"", 10));
+			
+			System.out.println((int) avg);
+		}
+	}
+
+	private String[] getDataDirs() {
+		
+		//Proper way would be to get it from each node
+		//Cluster with same configuration this is fine
+		String dataDirsParam = getConf().get("dfs.data.dir");
+		if(dataDirsParam == null) 
+			dataDirsParam = getConf().get("dfs.datanode.data.dir");
+		
+		String[] dataDirs = null;
+		
+		if(dataDirsParam == null){
+			LOG.warn("dfs.data.dir or dfs.datanode.data.dir "
+					+ "cofiguration parameter is not set, so block distribution can not be shown.");
+		}else{
+			dataDirs = dataDirsParam.split(",");
+		}
+		
+		return dataDirs;
+	}
+
+	private HashMap<String, HashMap<Integer, Integer>> calculateHostsDiskIdsCount(
+			BlockStorageLocation[] blockStorageLocations) throws IOException {
+		
+		HashMap<String, HashMap<Integer, Integer>> hosts_diskIds = new HashMap<>(); 
+		for (BlockStorageLocation blockStorageLocation : blockStorageLocations) {
+			String[] hosts = blockStorageLocation.getHosts();
+			VolumeId[] volumeIds = blockStorageLocation.getVolumeIds();
+			
+			for (int i = 0; i < hosts.length; i++) {
+				String host = hosts[i];
+				Integer diskId = getDiskId(volumeIds[0]);
+				
+				if(!hosts_diskIds.containsKey(host)){
+					HashMap<Integer, Integer> diskIds = new HashMap<>();
+					diskIds.put(diskId, 1);
+					hosts_diskIds.put(host, diskIds);
+				}else{
+					HashMap<Integer, Integer> diskIds = hosts_diskIds.get(host);
+					Integer count = diskIds.get(diskId + 1);
+					if(count != null){
+						diskIds.put(diskId, count + 1);
+					}else{
+						diskIds.put(diskId, 1);
+					}
+				}
+			}
+		}
+		
+		return hosts_diskIds;
+	}
+
+	private void printFileStatus(Path path) throws IOException {
+		FileStatus status = getFileStatus(path);
+
+		System.out.println();
+		System.out.println("Showing metadata for: " + status.getPath());
+		System.out.println("	isDirectory: " + status.isDirectory());
+		System.out.println("	isFile: " + status.isFile());
+		System.out.println("	isSymlink: " + status.isSymlink());
+		System.out.println("	encrypted: " + status.isEncrypted());
+		System.out.println("	length: " + status.getLen());
+		System.out.println("	replication: " + status.getReplication());
+		System.out.println("	blocksize: " + status.getBlockSize());
+		System.out.println("	modification_time: " + new Date(status.getModificationTime()));
+		System.out.println("	access_time: " + new Date(status.getAccessTime()));
+		System.out.println("	owner: " + status.getOwner());
+		System.out.println("	group: " + status.getGroup());
+		System.out.println("	permission: " + status.getPermission());
+		System.out.println();
+	}
+
+	private LinkedList<BlockLocation> getBlockLocations(Path path) throws IOException {
+		System.out.println("Collecting block locations...");
+		
+		LinkedList<BlockLocation> blockLocations = new LinkedList<BlockLocation>();
+		RemoteIterator<LocatedFileStatus> statuses = listFiles(path, true);
+		while(statuses.hasNext()){
+			LocatedFileStatus fileStatus = statuses.next();
+			
+			if(fileStatus.isFile()){
+				BlockLocation[] blockLocations_tmp = getFileBlockLocations(fileStatus, 0, fileStatus.getLen());
+				
+				blockLocations.addAll(Arrays.asList(blockLocations_tmp));
+			}
+			
+			int size = blockLocations.size();
+			if(size > 0 && size % 5000 == 0)
+				System.out.println("Collected " + size + " locations. Still in progress...");
+			
+			if(size >= MAX_NUMBER_OF_LOCATIONS){
+				System.out.println("Reached max number of locations to collect. The amount will be representative enough.");
+				break;
+			}
+		}
+		System.out.println("Collected " + blockLocations.size() + " locations.");
+		System.out.println();
+		
+		return blockLocations;
+	}
+
+	private static String adjustLength(String string, int length) {
+		if(string.length() > length)
+			return string.substring(0, length - 3).concat("...");
+
+		while(string.length() < length)
+			string = string.concat(" ");
+		
+		return string;
+	}
+
+	private static void printBlockMetadata(BlockLocation blockStorageLocation, String[] dataDirs) 
+			throws IOException {
+		
+		System.out.println("	Offset: " + blockStorageLocation.getOffset());
+		System.out.println("	Length: " + blockStorageLocation.getLength());
+
+		String[] cachedHosts = blockStorageLocation.getCachedHosts();
+		if (cachedHosts.length == 0) {
+			System.out.println("	No cached hosts");
+		}
+
+		System.out.println("	Hosts:");
+		VolumeId[] volumeIds = blockStorageLocation instanceof BlockStorageLocation ?
+				(((BlockStorageLocation) blockStorageLocation).getVolumeIds()) : null;
+		String[] hosts = blockStorageLocation.getHosts();
+		String[] names = blockStorageLocation.getNames();
+		String[] topologyPaths = blockStorageLocation.getTopologyPaths();
+		for (int i = 0; i < topologyPaths.length; i++) {
+			int diskId = volumeIds != null ? getDiskId(volumeIds[i]) : -1;
+			
+			System.out.println("		Replica (" + i + "):");
+			System.out.println("			Host: " + hosts[i]);
+			
+			if(diskId == -1)
+				System.out.println("			DiskId: unknown");
+			else if(dataDirs != null && diskId < dataDirs.length)
+				System.out.println("			Location: " + dataDirs[diskId] + " (DiskId: " + diskId + ")");
+			else
+				System.out.println("			DiskId: " + diskId);
+			
+			System.out.println("			Name: " + names[i]);
+			System.out.println("			TopologyPaths: " + topologyPaths[i]);
+		}
+
+		if (cachedHosts.length > 0) {
+			System.out.println("	Cached hosts:");
+			for (String cachedHost : cachedHosts) {
+				System.out.println("		Host: " + cachedHost);
+			}
+		}
+	}
+
+	/**
+	 * Returns a disk id (0-based) index from the Hdfs VolumeId object. There is
+	 * currently no public API to get at the volume id. We'll have to get it by
+	 * accessing the internals.
+	 */
+	private static int getDiskId(VolumeId hdfsVolumeId){
+		// Initialize the diskId as -1 to indicate it is unknown
+		int diskId = -1;
+
+		if (hdfsVolumeId != null) {
+			String volumeIdString = hdfsVolumeId.toString();
+
+			byte[] volumeIdBytes = StringUtils.hexStringToByte(volumeIdString);
+			if (volumeIdBytes != null && volumeIdBytes.length == 4) {
+				diskId = toInt(volumeIdBytes);
+			}else if (volumeIdBytes.length == 1) {
+				diskId = (int) volumeIdBytes[0];  // support hadoop-2.0.2
+	        }
+		}
+
+		return diskId;
+	}
+
+	/**
+	 * Converts a byte array to an int value
+	 * 
+	 * @param bytes
+	 *            byte array
+	 * @return the int value
+	 * @throws IllegalArgumentException
+	 *             if length is not {@link #SIZEOF_INT}
+	 */
+	public static int toInt(byte[] bytes) {
+		if (SIZEOF_INT > bytes.length)
+			throw new IllegalArgumentException("length is not SIZEOF_INT");
+		
+		int n = 0;
+		for (int i = 0; i < + bytes.length; i++) {
+			n <<= 8;
+			n ^= bytes[i] & 0xFF;
+		}
+		return n;
+	}
+
+}
