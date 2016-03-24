af153f2ef95cc3279016f48c88c128e54b153b78
Daniel Lanza Garcia <dlanza1@gmail.com>
2016-03-24 12:57:59 +0100

    Add copyright headers

--- src/main/java/ch/cern/db/util/Utils.java
+++ src/main/java/ch/cern/db/util/Utils.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) 2016, CERN
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".
+ * In applying this license, CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
 package ch.cern.db.util;
 
 public class Utils {
